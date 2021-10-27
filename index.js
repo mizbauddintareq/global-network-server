b2b69f322d8391f6c52f42947988924be7f65dea
mizbauddintareq <mizbauddintareq@gmail.com>
2021-10-27 17:47:32 +0600

    update

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rvmjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("volunteer_service");
+    const eventCollections = database.collection("events");
+
+    // POST volunteer service
+    app.post("/addServices", async (req, res) => {
+      const event = req.body;
+      const result = await eventCollections.insertOne(event);
+      res.send(result);
+    });
+
+    // GET all services
+    app.get("/allEvents", async (req, res) => {
+      const cursor = await eventCollections.find({}).toArray();
+      res.send(cursor);
+    });
+  } finally {
+    // await client?.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
